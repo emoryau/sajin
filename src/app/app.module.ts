73bf106c153758cb5502a63c09d384f813663003
emoryau <emoryau@gmail.com>
2019-03-04 15:53:54 -0500

    Added viewer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { ViewerComponent } from './viewer/viewer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GalleryComponent
+    GalleryComponent,
+    ViewerComponent
   ],
   imports: [
     BrowserModule,
